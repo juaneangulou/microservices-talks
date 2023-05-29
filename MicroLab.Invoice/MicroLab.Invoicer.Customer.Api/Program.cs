e567220c4bde4dd84805fdbf53bea3eec929fc40
Juan Esteban Angulo Uribe <jangulorama@gmail.com>
2023-05-29 15:13:46 -0500

    New fixes about new  db server and  some configurations

--- MicroLab.Invoice/MicroLab.Invoicer.Customer.Api/Program.cs
+++ MicroLab.Invoice/MicroLab.Invoicer.Customer.Api/Program.cs
@@ -18,13 +18,13 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
